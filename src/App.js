b9c36c65eda436ab4f99d0a90cf95bfdf312b80d
voinot <flovno@gmail.com>
2020-04-02 17:47:32 +0200

    finish excercise

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import Contact from './components/Contact'
+
 import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact avatar='https://randomuser.me/api/portraits/men/1.jpg'
-        name='Liam Douglas'
-        online />
-      <Contact avatar='https://randomuser.me/api/portraits/men/3.jpg'
-        name='Marvin Dean' />
-      <Contact avatar='https://randomuser.me/api/portraits/men/79.jpg'
-        name='Nathan Gordon'
-        online />
       <ContactList/>
     </div>
   );
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,47 +1,48 @@
 import React from 'react'
-import './Contact.css'
-import PropTypes from 'prop-types'
-
-
+import Contact from './Contact'
 
 
 const users = [
     {
-      name: 'Robert Reyes',
-      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
+        name: 'Robert Reyes',
+        avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+        online: false
     },
     {
-      name: 'Nellie Caldwell',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
+        name: 'Nellie Caldwell',
+        avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+        online: true
     },
     {
-      name: 'Vernon Mason',
-      avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
+        name: 'Vernon Mason',
+        avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+        online: true
     },
     {
-      name: 'Erica Hunt',
-      avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
+        name: 'Erica Hunt',
+        avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+        online: false
     },
     {
-      name: 'Juanita Phillips',
-      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
+        name: 'Juanita Phillips',
+        avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+        online: true
     }
-  ];
+];
 
-  const ContactList = () => (
+const ContactList = () => (
     <div>
-      {users.map(item => (
-        <ContactList
-          name={item.name}
-          avatar={item.avatar}
-          online={item.online}
-        />
-      ))}
+        {users.map(item => (
+            <Contact
+                avatar={item.avatar}
+                name={item.name}
+                online={item.online}
+            />
+        ))}
     </div>
-  );
-  export default ContactList
+)
+
+
+
+
+export default ContactList
